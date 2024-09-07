2390fdcfefa31f24a9cac8a2134faf8a45122111
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-09-07 23:57:13 +0100

    code

--- commit_v2.py
+++ commit_v2.py
@@ -33,7 +33,7 @@ if not file:
     for file in changed_files:
         repo.git.add(file)
         repo.git.commit('-m', message)
-        repo.push()
+        repo.git.push()
         print('None')
 
     print(changed_files)
