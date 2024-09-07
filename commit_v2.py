40be1a261a796d3c9360adf7c9a493b13f99839c
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-09-07 23:32:20 +0100

    code

--- commit_v2.py
+++ commit_v2.py
@@ -23,7 +23,7 @@ if not file:
     try:
         repo = git.Repo(cwd)
     except git.exc.InvalidGitRepositoryError as e:
-        new_cwd = e.args[0]
+        new_cwd = e.args[0].split('/')[3]
         print(new_cwd)
         exit(1)
 
