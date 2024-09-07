ed4aba2462e9cc18522e8685af7acfc8aaca9730
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-09-07 23:40:02 +0100

    code

--- commit_v2.py
+++ commit_v2.py
@@ -23,7 +23,6 @@ if not file:
     try:
         repo = git.Repo(cwd)
     except git.exc.InvalidGitRepositoryError as e:
-        path = os.getcwd().find()
         dir = e.args[0].split('/')[3]
         repo = git.Repo(f'/home/tony/{dir}')
 
